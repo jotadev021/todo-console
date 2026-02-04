3a725b1e3683963cb3dd298e9c36882e737f2003
Juan Pablo <juancora225@gmail.com>
2026-02-04 04:27:02 -0300

    Adiciona comentário explicativo sobre objetivo do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+/*
+ Projeto criado com o objetivo de praticar:
+ - Estrutura básica de um programa Java
+ - Compilação e execução via linha de comando
+ - Uso inicial do Git e GitHub
+ - Organização de projeto e boas práticas iniciais
+
+ Este projeto serve como base para evoluções futuras,
+ incluindo menu interativo e persistência de dados.
+*/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("To-Do Console rodando!");
